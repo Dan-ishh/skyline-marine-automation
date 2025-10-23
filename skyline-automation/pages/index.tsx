68d8dc7c36584688994de7af73fa6efbc9b983f9
Danish <hey.danishsheikh@gmail.com>
2025-10-23 23:20:55 +0530

    updated from app router to page router

--- skyline-automation/pages/index.tsx
+++ skyline-automation/pages/index.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import styles from "./page.module.css";
+import styles from "./index.module.css";
 
 export default function Home() {
   return (
@@ -14,7 +14,7 @@ export default function Home() {
           priority
         />
         <div className={styles.intro}>
-          <h1>To get started, edit the page.tsx file.</h1>
+          <h1>To get started, edit the pages/index.tsx file.</h1>
           <p>
             Looking for a starting point or more instructions? Head over to{" "}
             <a
